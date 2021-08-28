c0829782deb6391df9172b9e4b95f280e3cdf477
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-08-28 17:43:28 -0300

    styles: add style guide

--- src/server.ts
+++ src/server.ts
@@ -6,4 +6,4 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-app.listen(3333, () => console.log('Server started on port 3333!'))
+app.listen(3333, () => console.log('Server started on port 3333!'));
