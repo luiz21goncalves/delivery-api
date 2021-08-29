07904fdfbc3199154506e55c391d815857113765
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-08-29 12:54:42 -0300

    feat: add custom logger

---
+++ src/logger.ts
@@ -0,0 +1,15 @@
+import pino from 'pino';
+
+import { name } from '../package.json';
+
+const [date] = new Date().toISOString().split(/T/g);
+
+const logger = pino({
+  name,
+  prettyPrint: {
+    colorize: false,
+    singleLine: false,
+  },
+}, pino.destination(`./logs/app/${date}.log`));
+
+export { logger };
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,30 @@
-import express from 'express';
 import cors from 'cors';
+import express from 'express';
+import expressPinoLogger from 'express-pino-logger';
+import pino from 'pino';
+
+import { logger } from './logger';
 
 const app = express();
+
+const [date] = new Date().toISOString().split(/T/g);
+
+app.use(expressPinoLogger({
+  prettyPrint: {
+    colorize: false,
+    singleLine: true,
+  },
+}, pino.destination(`./logs/request/${date}.log`)));
 
 app.use(express.json());
 app.use(cors());
 
-app.listen(3333, () => console.log('Server started on port 3333!'));
+app.get('/', (request, response) => {
+  logger.info('test route');
+
+  return response.json({
+    ok: true,
+  });
+});
+
+app.listen(3333, () => logger.info('Server started on port 3333!'));
