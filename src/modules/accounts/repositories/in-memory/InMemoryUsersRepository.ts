edfb39bebc7870eb8f655f5f0526382b4d89e992
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-08-29 18:34:53 -0300

    feat: add in memory users repository

---
+++ src/modules/accounts/repositories/in-memory/InMemoryUsersRepository.ts
@@ -0,0 +1,43 @@
+import { User } from '@modules/accounts/infra/typeorm/entities/User';
+import { ICreateUserDTO } from '@modules/accounts/useCases/ICreateUserDTO';
+import { logger } from '@shared/logger';
+
+import { IUsersRepository } from '../IUsersRepository';
+
+class InMemoryUsersRepository implements IUsersRepository {
+  private users: User[];
+
+  constructor() {
+    this.users = [];
+  }
+
+  async create({ name, email, password }: ICreateUserDTO): Promise<User> {
+    logger.debug({
+      class: 'InMemoryUsersRepository',
+      fn: 'create',
+      args: { name, email, password },
+    });
+
+    const user = new User();
+
+    Object.assign(user, {
+      name,
+      email,
+      password,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    this.users.push(user);
+
+    logger.debug({
+      class: 'InMemoryUsersRepository',
+      fn: 'create',
+      user,
+    });
+
+    return user;
+  }
+}
+
+export { InMemoryUsersRepository };
