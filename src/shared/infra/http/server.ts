ec1dd267f8d30c08d0e2831bfea8474e1086f448
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-08-30 19:19:32 -0300

    refactor: separete app from server

--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,59 +1,10 @@
-import 'dotenv/config';
 import 'reflect-metadata';
+import 'dotenv/config';
 
-import cors from 'cors';
-import express, { Request, Response, NextFunction } from 'express';
-import 'express-async-errors';
-import expressPinoLogger from 'express-pino-logger';
-import pino from 'pino';
-
-import { AppError } from '@shared/errors/AppError';
 import { logger } from '@shared/logger';
-import '@shared/container';
 
-import { createDatabaseConnetion } from '../typeorm';
-import { routes } from './routes';
-
-const app = express();
-
-createDatabaseConnetion();
-
-const [date] = new Date().toISOString().split(/T/g);
-
-app.use(
-  expressPinoLogger(
-    {
-      prettyPrint: {
-        colorize: false,
-        singleLine: true,
-      },
-    },
-    pino.destination(`./logs/request/${date}.log`),
-  ),
-);
-
-app.use(express.json());
-app.use(cors());
-app.use(routes);
+import { app } from './app';
 
 const port = process.env.PORT ?? 3333;
-
-app.use(
-  (err: Error, resquest: Request, response: Response, _: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        error: err.message,
-        statusCode: err.statusCode,
-      });
-    }
-
-    logger.error({ middleware: 'Error Middleware', err });
-
-    return response.status(500).json({
-      message: 'Internal server error',
-      statusCode: 500,
-    });
-  },
-);
 
 app.listen(port, () => logger.info(`Server started on port ${port}!`));
