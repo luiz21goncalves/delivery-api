d7349a0ccddf4d2c9484f5c64e6c462595269f79
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-09-01 21:52:38 -0300

    feat: add production config

--- ormconfig.js
+++ ormconfig.js
@@ -5,11 +5,21 @@ const config = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  entities: ['./src/modules/**/entities/*.ts'],
-  migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+  entities: ['./dist/modules/**/entities/*.ts'],
+  migrations: ['./dist/shared/infra/typeorm/migrations/*.ts'],
   cli: {
-    migrationsDir: './src/shared/infra/typeorm/migrations',
+    migrationsDir: './dist/shared/infra/typeorm/migrations',
   },
 };
+
+if (process.env.NODE_ENV !== 'production') {
+  Object.assign(config, {
+    entities: ['./src/modules/**/entities/*.ts'],
+    migrations: ['./src/shared/infra/typeorm/migrations/*.ts'],
+    cli: {
+      migrationsDir: './src/shared/infra/typeorm/migrations',
+    },
+  });
+}
 
 module.exports = config;
