f75bf6cac4e11c3ee6c6d788746ead7369f27fd1
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-09-05 10:18:23 -0300

    chore: deleve override TFunction

--- src/@types/i18next.d.ts
+++ src/@types/i18next.d.ts
@@ -1,7 +1,5 @@
 declare namespace i18next {
   type Keys = import('../shared/dtos/i18next.keys').TranslationKeys;
-  type TFunction = import('./i18next.overrides').TFunction;
 
   export type ResourceKey = Keys;
-  export type TFunction = TFunction;
 }
