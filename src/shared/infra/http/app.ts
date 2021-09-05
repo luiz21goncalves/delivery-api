4f2ff5e3cd23054c219c180fa4237fdc24445797
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-09-05 08:25:54 -0300

    chore: add middleware generateException

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 
 import cors from 'cors';
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 import 'express-async-errors';
 import expressPinoLogger from 'express-pino-logger';
 import i18next from 'i18next';
@@ -9,11 +9,10 @@ import i18nextMiddleware from 'i18next-http-middleware';
 import Backend from 'i18next-node-fs-backend';
 import pino from 'pino';
 
-import { AppError } from '@shared/errors/AppError';
-import { logger } from '@shared/logger';
 import '@shared/container';
 
 import { createDatabaseConnetion } from '../typeorm';
+import { generateException } from './middlewares/generateException';
 import { routes } from './routes';
 
 const app = express();
@@ -24,6 +23,7 @@ i18next
   .init({
     backend: {
       loadPath: `${__dirname}/../../resources/locales/{{lng}}/{{ns}}.json`,
+      addPath: 'locales/{{lng}}/{{ns}}.missing.json',
     },
     fallbackLng: 'en',
     preload: ['pt-BR', 'en'],
@@ -49,23 +49,6 @@ app.use(express.json());
 app.use(cors());
 app.use(i18nextMiddleware.handle(i18next));
 app.use(routes);
-
-app.use(
-  (err: Error, resquest: Request, response: Response, _: NextFunction) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        error: err.message,
-        statusCode: err.statusCode,
-      });
-    }
-
-    logger.error({ middleware: 'Error Middleware', err });
-
-    return response.status(500).json({
-      message: 'Internal server error',
-      statusCode: 500,
-    });
-  },
-);
+app.use(generateException);
 
 export { app };
