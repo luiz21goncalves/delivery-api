451e158f057d372999a09c9ab90aae382c7a0bf4
Luiz Gonçalves <luizhbgoncalves@gmail.com>
2021-09-05 08:25:54 -0300

    chore: add middleware generateException

---
+++ src/shared/infra/http/middlewares/generateException.ts
@@ -0,0 +1,29 @@
+import { Request, Response, NextFunction } from 'express';
+
+import { AppException } from '@shared/exceptions/AppException';
+import { logger } from '@shared/logger';
+
+function generateException(
+  err: Error,
+  request: Request,
+  response: Response,
+  _: NextFunction,
+): Response {
+  if (err instanceof AppException) {
+    logger.error({ middleware: 'app exception', err });
+
+    return response.status(err.statusCode).json({
+      message: request.t(err.message),
+      statusCode: err.statusCode,
+    });
+  }
+
+  logger.error({ middleware: 'internal exception', err });
+
+  return response.status(500).json({
+    message: request.t('internal_server_error'),
+    statusCode: 500,
+  });
+}
+
+export { generateException };
